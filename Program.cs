0fc75114f5e7a0aa21fd7ffe4b3dce2576b9cd4d
Yogesh <yogeshbietjhs@gmail.com>
2022-06-28 15:52:24 +0530

    inicial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LinkListU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkList list = new LinkList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+
+
+        }
+    }
+}
